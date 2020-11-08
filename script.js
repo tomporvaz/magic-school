33627c4a52ccfa12e90e3a4406b97c24b7e54bc2
tomporvaz <tomporvaz@gmail.com>
2020-11-07 23:40:17 -0500

    Completed Social Studies page, and enhanced side bar to highlight active subject.

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 $(function() {
-/*   $(".chevron-open").click(function () {
-    $('.sidebar').css('width', '320px');
-    //$('.chevron-close').hide();
-  }) */
-
-  $(".chevron-close").click(function() {
-    console.log("CLOSE")
-    //$('.sidebar').css('width', '70px')
-    //$('.chevron-open').show();
-  })
-
   $(".chevron-open").click(
     function() {
       $(this).hide();
-      console.log('OPEN')
       $('.sidebar').css('width', '320px');
-
     }
   )
 
   $(".chevron-close").click(
     function() {
-      console.log('CLOSE');
       $('.sidebar').css('width', '70px')
       $(".chevron-open").show();
     }
   )
+
+  $("#button-answer-social-studies").click(
+    function() {
+      console.log("SHOW.HIDE")
+      $("#answer-to-social-studies").show();
+    }
+  )
+
+
 });
 
 
